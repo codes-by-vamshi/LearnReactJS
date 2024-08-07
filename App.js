95b37c7678f4e773bdbb38b728a25e9916cfb03e
codes-by-vamshi <krishnavamshidevarasetty@gmail.com>
2024-08-07 19:17:28 +0530

    Creating nested React Elements

--- App.js
+++ App.js
@@ -1,3 +1,16 @@
-const heading = React.createElement("h1", { id: "heading" }, "Hello World! - from ReactJS");
+//const heading = React.createElement("h1", { id: "heading" }, "Hello World! - from ReactJS");
+const parent = React.createElement(
+    "div",
+    { id: "parent" },
+    React.createElement(
+        "div",
+        { id: "child" },
+        React.createElement(
+            "h1",
+            {},
+            "I'm an H1 Tag Element"
+        )
+    )
+)
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
