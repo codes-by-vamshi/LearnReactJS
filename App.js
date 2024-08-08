0d038e014754b1d8b6d9c228f12ca206a724e828
codes-by-vamshi <krishnavamshidevarasetty@gmail.com>
2024-08-08 23:23:50 +0530

    Used Prettier extension to format the code

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // Creating parent using JSX
-const parent =
-        <div id="parent">
-        <div id="child">
-            <h1>I'm an H1 Tag Element - Using JSX</h1>
-            <h2>I'm an H2 Tag Element - Using JSX</h2>
-        </div>
-    </div>;
+const parent = (
+  <div id="parent">
+    <div id="child">
+      <h1>I'm an H1 Tag Element - Using JSX</h1>
+      <h2>I'm an H2 Tag Element - Using JSX</h2>
+    </div>
+  </div>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
